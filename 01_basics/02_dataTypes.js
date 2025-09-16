97dceee0eb445854f41fe72ca3014bda7f6a6506
Rohit Borse <100746004+Rohit123borse@users.noreply.github.com>
2025-09-16 02:13:07 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict";// treat all JS code as newer version
+
+// alert(3 + 3) //we are using nodejs, not browswer
+
+console.log(3 + 
+    3);// code readability should be high
+
+console.log("Rohit")
+
+let name ="Rohit"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint  // bhot jyada number hoto bigint ka use hota hai like fesbook, stockmarket, trading 
+//string =>  ""
+//boolean => true/false
+//null => standalone value 
+//undefined =>
+//symbo => unique
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // boject
+
+/* 
+   Assignment specipcation ko deko
+   tc39.ec
+   
+   */
+
+
