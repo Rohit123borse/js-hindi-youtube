5b567024652411f9abe78d467c291f04a3516a10
Rohit Borse <100746004+Rohit123borse@users.noreply.github.com>
2025-09-25 06:31:26 +0000

    Oprations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -23,5 +23,24 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//**************************Operation*************************
+
+let value = 3
+let negValue = - value 
+// console.log(negValue);
+
+/*console.log(2+2);
+console.log(2-2);
+conaole.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);*/
+
+let str1 = "Hello"
+let str2 =" Rohit"
+
+let str3=str1+str2
+console.log(str3);
