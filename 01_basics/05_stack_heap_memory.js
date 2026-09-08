6a34a9a57a5afe3134171949708b93f7bb05bf80
Rohit Borse <100746004+Rohit123borse@users.noreply.github.com>
2026-09-08 12:05:15 +0000

    stack and heap memory examples and code clear

---
+++ 01_basics/05_stack_heap_memory.js
@@ -0,0 +1,23 @@
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack(primitive), Heap(Non-primitive)
+
+let myYoutubename ="MyYoutubechannal.com"
+
+let anothername = myYoutubename
+anothername ="chaiaurcode"
+
+console.log(myYoutubename)
+console.log(anothername)
+
+
+let userOne ={
+    email:"user@gmail.com",
+    upi:"user@ypl"
+}
+
+let userTwo = userOne
+userTwo.email="rohiltborse@gmail.com",
+
+console.log(userOne.email)
+console.log(userTwo.email)
